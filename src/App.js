88eac591d42254b250b86e0230f714beec21f2c6
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-06-13 14:05:40 -0700

    Implemented header menu bottom bar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import HeaderTop from './components/header/HeaderTop'
+import HeaderBottom from './components/header/HeaderBottom'
+
 function App() {
   return (
     <div className="App">
         <HeaderTop/>
+        <HeaderBottom/>
     </div>
   );
 }
