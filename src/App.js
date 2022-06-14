dc972db3e333d012cc74762798aa0a76172f2e07
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-06-14 10:46:34 -0700

    Implemented mock login user page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderTop from './components/header/HeaderTop'
 import HeaderBottom from './components/header/HeaderBottom'
+import Login from './components/login/Login'
 
 function App() {
   return (
     <div className="App">
         <HeaderTop/>
         <HeaderBottom/>
+        <Login/>
     </div>
   );
 }
