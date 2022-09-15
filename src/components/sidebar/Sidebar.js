86cbb757e85de247fb4cd7146393b165b80eb0df
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-09-14 17:57:46 -0700

    Added title and text

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -20,9 +20,45 @@ export default function Sidebar() {
                 <a>Best Sellers</a>
                 <a>New Releases</a>
                 <a>Movers & Shakers</a>
-            </div>
+            
             <div className="menu_divider"></div>
-
+                <h4>
+                    Digital Content And Devices
+                </h4>
+                <a>Echo & Alexia</a>
+                <a>Fire Tablets & Fire TV</a>
+                <a>Kindle</a>
+                <a>Audible Audiobooks</a>
+                <a>Amazon Prime Video</a>
+                <a>Amazon Music</a>
+                <a>Appstore for Android</a>
+                <div className="menu_divider"></div>
+                <h4>
+                    Shop By Department
+                </h4>
+                <a>Books</a>
+                <a>Video Geams & Prime Gaming</a>
+                <a>Music, Movies & TV Shows</a>
+                <a>Electronics, Computers & Office</a>
+                <a>Sell All</a>
+                <h4>
+                    Programs & Features
+                </h4>
+                <a>Amazon Prime</a>
+                <a>Savings Programs</a>
+                <a>Gift Cards</a>
+                <a>Boutiques Francophones</a>
+                <a>See All</a>
+                <div className="menu_divider"></div>
+                <h4>
+                    Help & Settings
+                </h4>
+                <a>Your Account</a>
+                <a>English</a>
+                <a>Canada</a>
+                <a>Help</a>
+                <a>Sign in</a>
+            </div>
         </div>
 
 
