c50c1b85e9606ceb30a93266f68a4fe763db0449
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-09-14 19:25:23 -0700

    Added icons

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
 import Avatar from '../../assets/amazon-profile-icon.png'
+import GrayArrow from '../../assets/amazon-gray-arrow-icon.png'
+import DarkArrow from '../../assets/amazon-dark-arrow-icon.png'
+import GrayDownArrow from '../../assets/amazon-gray-down-arrow-icon.png'
+import Globe from '../../assets/amazon-globe-icon.png'
+import Flag from '../../assets/amazon-mini-canada-flag.png'
+import Cancel from '../../assets/amazon-cancel-icon.png'
 export default function Sidebar() {
   return (
     <div className="sidebar-container">
+        <img src={Cancel} className='sidebar-cancel'/>
         <div className="sidebar-header">
             <div className='sidebar-avatar'>
                 <img src={Avatar}/>
@@ -25,37 +32,37 @@ export default function Sidebar() {
                 <h4>
                     Digital Content And Devices
                 </h4>
-                <a>Echo & Alexia</a>
-                <a>Fire Tablets & Fire TV</a>
-                <a>Kindle</a>
-                <a>Audible Audiobooks</a>
-                <a>Amazon Prime Video</a>
-                <a>Amazon Music</a>
-                <a>Appstore for Android</a>
+                <a><div>Echo & Alexia <img src={GrayArrow}/></div></a>
+                <a><div>Fire Tablets & Fire TV <img src={GrayArrow}/></div></a>
+                <a><div> Kindle <img src={GrayArrow}/></div></a>
+                <a><div> Audible Audiobooks <img src={GrayArrow}/></div></a>
+                <a><div> Amazon Prime Video <img src={GrayArrow}/></div></a>
+                <a><div> Amazon Music <img src={GrayArrow}/></div></a>
+                <a><div> Appstore for Android <img src={GrayArrow}/></div></a>
                 <div className="menu_divider"></div>
                 <h4>
                     Shop By Department
                 </h4>
-                <a>Books</a>
-                <a>Video Geams & Prime Gaming</a>
-                <a>Music, Movies & TV Shows</a>
-                <a>Electronics, Computers & Office</a>
-                <a>Sell All</a>
+                <a><div> Books <img src={GrayArrow}/></div></a>
+                <a><div> Video Geams & Prime Gaming <img src={GrayArrow}/></div></a>
+                <a><div> Music, Movies & TV Shows <img src={GrayArrow}/></div></a>
+                <a><div> Electronics, Computers & Office <img src={GrayArrow}/></div></a>
+                <a> Sell All <img src={GrayDownArrow}/></a>
                 <h4>
                     Programs & Features
                 </h4>
-                <a>Amazon Prime</a>
-                <a>Savings Programs</a>
-                <a>Gift Cards</a>
-                <a>Boutiques Francophones</a>
-                <a>See All</a>
+                <a><div> Amazon Prime <img src={GrayArrow}/></div></a>
+                <a><div> Savings Programs <img src={GrayArrow}/></div></a>
+                <a><div> Gift Cards <img src={GrayArrow}/></div></a>
+                <a><div> Boutiques Francophones <img src={GrayArrow}/></div></a>
+                <a> See All <img src={GrayDownArrow}/></a>
                 <div className="menu_divider"></div>
                 <h4>
-                    Help & Settings
+                 Help & Settings
                 </h4>
                 <a>Your Account</a>
-                <a>English</a>
-                <a>Canada</a>
+                <a><img src={Globe} style={{width: '15px', height: '15px', marginLeft: '0', marginRight: '10px'}}/>English</a>
+                <a><img src={Flag} style={{width: '20px', height: '15px', marginLeft: '0', marginRight: '5px'}}/>Canada</a>
                 <a>Help</a>
                 <a>Sign in</a>
             </div>
