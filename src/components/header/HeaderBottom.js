f1daa9e75895a7256afee1245e1b03e7422f2da7
Darren <darren277@yahoo.com>
2022-09-23 13:19:26 -0700

    Removed the arbitrarily defined widths from margin bottom. We will have to come up with another way to achieve responsiveness.

--- src/components/header/HeaderBottom.js
+++ src/components/header/HeaderBottom.js
@@ -15,16 +15,16 @@ function HeaderBottom() {
                 <a className="header-menu-content">Best Sellers</a>
                 <a className="header-menu-content">Customer Service</a>
                 <a className="header-menu-content">Deals Store</a>
-                <a className="header-menu-content width-670">New Releases</a>
-                <a className="header-menu-content width-840">Prime</a>
-                <img className="header-menu-content-dropdown width-840" src={require("../../assets/amazon-icons-dropdown-arrow-grey.png")} alt="amazon-dropdown"/>
-                <a className="header-menu-content width-840">Fashion</a>
-                <a className="header-menu-content width-840">Sell</a>
-                <a className="header-menu-content width-1252">Electronics</a>
-                <a className="header-menu-content width-1252">Home</a>
-                <a className="header-menu-content width-1252">Books</a>
-                <a className="header-menu-content width-1252">Toys & Games</a>
-                <a className="header-menu-content width-1252">Computers</a>
+                <a className="header-menu-content">New Releases</a>
+                <a className="header-menu-content">Prime</a>
+                <img className="header-menu-content-dropdown" src={require("../../assets/amazon-icons-dropdown-arrow-grey.png")} alt="amazon-dropdown"/>
+                <a className="header-menu-content">Fashion</a>
+                <a className="header-menu-content">Sell</a>
+                <a className="header-menu-content">Electronics</a>
+                <a className="header-menu-content">Home</a>
+                <a className="header-menu-content">Books</a>
+                <a className="header-menu-content">Toys & Games</a>
+                <a className="header-menu-content">Computers</a>
             </div>
             <div className="header-menu-right">
                 <img className="header-menu-right-height" src={require("../../assets/amazon-prime.jpg")}/>
